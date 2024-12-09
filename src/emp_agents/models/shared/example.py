4fde8f50298df2e7912c9c47a83ab2f8b6cd547b
Lilypad Forest <lilypad@forest.com>
2024-12-09 19:28:41 +0000

    add example classes

---
+++ src/emp_agents/models/shared/example.py
@@ -0,0 +1,7 @@
+from emp_agents.models.shared.message import AssistantMessage, ToolMessage, UserMessage
+
+
+QuestionAnswer = tuple[UserMessage, AssistantMessage]
+TooledMessageSequence = tuple[UserMessage, AssistantMessage, list[ToolMessage]]
+
+Example = QuestionAnswer | TooledMessageSequence
